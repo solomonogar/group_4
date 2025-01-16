e24c3b4278d7d3168d24a18c7f1b195c130a0883
Goodness Waino <goodnessjohnwaino@gmail.com>
2025-01-16 13:09:42 +0100

    started work on home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Project from './pastproject/Project'
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Welcome Home</div>
-  )
+    <div>
+      <div className='home-area'>
+          <div className="welcome__contianer">
+            <span> <h1 className="welcome">Wendy Ju</h1></span>
+          </div>
+           
+      </div>
+      <div>
+        <Project /> 
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
