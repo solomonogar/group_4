719df02b57a03b60b43d2b822dd8dcc70916e1db
Goodness Waino <goodnessjohnwaino@gmail.com>
2025-01-27 14:10:32 +0100

    Remove font import from index.html and modify App.css

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,9 +5,6 @@ import noun from '../../assets/noun.png'
 import adj from  '../../assets/adj.png'
 import verb from '../../assets/vreb.png'
 import first from '../../assets/image.jpg'
-import second from '../../assets/image2.png'
-import third from '../../assets/image3.png'
-import forth from '../../assets/image4.png'
 
 const Home = () => {
   return (
@@ -33,47 +30,9 @@ const Home = () => {
             raises thoughtful<br/> questions to challenge<br/> norms
         </div>
         </div>  
-        <div className='client-contianer'>
-          <div className='client-main'>
-            <p className='client'><b>Client: inQ Services</b></p>
-            <div className='increase'>Increasing operational efficiency in facilities management</div>
-            <div className='product'>0-to-1 Product Design   |   SaaS Platform  <br/>|  Web Design  |
-            AppDesign  |<br/>  Stakeholder Management</div>
-          </div>
+        <div>
           <img className='fisrtpic' src={first} alt='picture of a screen'/>
-        </div>
-        <div className='empower-contianer'>
-            <img className='secondpic' src={second} alt='picture of a screen'/>
-          <div className='empower-main'>
-            <div className='empower'>Empowering female solo<br/> travelers with <br/>
-            trustworthy and <br/>
-              relevant safety<br/> information</div>
-            <div className='product'> Web Design  
-               |   Material Design   |   UX <br/> Design</div>
-           </div>
         </div> 
-        <div className='enhance-contianer'>
-     <div className='enhance-main'>
-          <p className='client'><b>Client: Guardian Life</b></p>
-
-          <div className='increase'>Enhancing financial planning 
-          experience through rebranding</div> 
-          <div className='product'> Visual Design  |   Responsive Design
-              | <br/> Rebranding</div>
-      </div>    
-          <img className='thirdpic' src={third} alt='a picture of a man smiling'/>
-        </div>
-        <div className='increase-contianer'>
-          <img className='forthpic' src= {forth} alt='picture of a phone' />
-          <div className='increase-main'>
-          <div className='increasing'>Increasing motivation<br/> in young adults
-           to form<br/> long 
-          term<br/> habits through<br/> gamification</div>
-          <div className='product'>Physical Product   |   App Design   | <br/>
-            Interaction Design   |   UX Research</div>
-          </div>
-        </div>
-
       </div>
       <div>
         <Project /> 
