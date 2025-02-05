06b62a262838658e39e163f0f87e10ee7ff53c01
Augustine Chinaemerem Eneh <enehaugustine01@gmail.comm>
2025-02-05 01:00:55 +0100

    fixed and updated the Aboutpage

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,7 @@
- import React from "react";
+import React from "react";
 import "./About.css";
 const About = () => {
   return (
-    <div>About</div>
     <>
       <main>
         <div className="banner">
@@ -124,7 +123,7 @@ const About = () => {
               </a>
             </p>
           </div>
-         </section>
+        </section>
       </main>
     </>
   );
